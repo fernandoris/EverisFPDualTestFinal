360015ab86da6fbd93519c2b3e6044a84c5f258a
fox1210 <pablogomezmelendez@gmail.com>
2020-06-10 11:44:15 +0200

    Clonacion del proyecto final

--- src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
+++ src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
@@ -8,15 +8,41 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.UniqueElements;
+import org.springframework.lang.NonNull;
 
 @Entity
 @Table(name = "users")
-public class Usuario implements Serializable{
-	
+public class Usuario implements Serializable {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name = "email")
+	@NonNull
+	@UniqueElements
+	@Size(min = 0, max = 64)
+	private String email;
+
+	@Column(name = "firstname ")
+	@NonNull
+	@Size(min = 0, max = 64)
+	private String fisfirstname;
+
+	@Column(name = "lastname ")
+	@NonNull
+	@Size(min = 0, max = 64)
+	private String lastname;
+
+	@Column(name = "password  ")
+	@NonNull
+	@Size(min = 0, max = 64)
+	private String password;
 	private static final long serialVersionUID = 1L;
-	
-	//Enunciado: Desarrolla la entidad para obtener los datos de BBDD
-	
-	
-	
+
+	// Enunciado: Desarrolla la entidad para obtener los datos de BBDD
+
 }
