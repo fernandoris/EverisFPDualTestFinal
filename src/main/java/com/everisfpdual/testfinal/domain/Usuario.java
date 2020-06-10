d7a12339d74ffcf55cf8d5e51cfd151ed057cad1
alicia <agarcluq@everis.com>
2020-06-10 10:17:01 +0200

    PRIMER COMMIT

--- src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
+++ src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
@@ -8,6 +8,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "users")
@@ -16,6 +18,31 @@ public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	//Enunciado: Desarrolla la entidad para obtener los datos de BBDD
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	
+	@Column(name="email",unique=true) 
+	@NotNull
+	@Size(min = 1, max = 64)
+	private String email;
+	
+	@Column(name="firstname",unique=true) 
+	@NotNull
+	@Size(min = 1, max = 64)
+	private String firstname;
+	
+	@Column(name="lastname",unique=true) 
+	@NotNull
+	@Size(min = 1, max = 64)
+	private String lastname;
+	
+	@Column(name="password",unique=true) 
+	@NotNull
+	@Size(min = 1, max = 64)
+	private String password;
 	
 	
 	
