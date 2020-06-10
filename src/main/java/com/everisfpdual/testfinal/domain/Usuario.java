3bb197f3294dd5e71e39329c344d6cfa77ee0e7a
salva <smunozco@everis.com>
2020-06-10 12:14:35 +0200

    Añadido código en Usuario.java y TestFinalServiceImpl.java
    añadidos getters y setters en usuario y empezar a desarrollar funcionalidad getExcel()

--- src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
+++ src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
@@ -38,6 +38,49 @@ public class Usuario implements Serializable{
 	@Column(name="password", nullable=false)
 	@Size(min=10,max=64)
 	private String password;
-	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
 	
 }
