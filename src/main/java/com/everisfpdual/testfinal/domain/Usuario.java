ab1d635c507054b9d355e798a84a8ef6cd94b0bb
Francisco García <garmacfco@gmail.com>
2020-06-11 17:29:23 +0200

    Parte 4 : Excel
    Creacion del excel con la información de la base de datos (la descarga no se produce)

--- src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
+++ src/main/java/com/everisfpdual/testfinal/domain/Usuario.java
@@ -33,4 +33,55 @@ public class Usuario implements Serializable{
 	
 	@Column(name="password")
 	private String password;
+
+	public Usuario(int id, String email, String firstname, String lastname, String password) {
+		this.id = id;
+		this.email = email;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.password = password;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
 }
--- src/main/java/com/everisfpdual/testfinal/serviceimpl/TestFinalServiceImpl.java
+++ src/main/java/com/everisfpdual/testfinal/serviceimpl/TestFinalServiceImpl.java
@@ -1,22 +1,34 @@
 package com.everisfpdual.testfinal.serviceimpl;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-/*
+
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.util.IOUtils;
 import org.apache.poi.xssf.usermodel.XSSFFont;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-*/
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -26,37 +38,123 @@ import com.everisfpdual.testfinal.domain.Usuario;
 import com.everisfpdual.testfinal.repository.UsuarioRepository;
 import com.everisfpdual.testfinal.service.TestFinalService;
 import com.everisfpdual.testfinal.util.Constant;
-//import com.opencsv.CSVReader;
+import com.opencsv.CSVReader;
 
 @Service
-public class TestFinalServiceImpl implements TestFinalService{
+public class TestFinalServiceImpl implements TestFinalService {
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
-	
+
 	public ByteArrayInputStream getExcel() {
 		
 		//Enunciado: Obtener lista de Usuarios e implementar la llamada al metodo para obtener el excel
 		List<Usuario> usuarios = new ArrayList<>();
-		ByteArrayInputStream inputStreamResource = null;
+		String[] columnas = {"Id", "Correo", "Nombre", "Apellido", "Contraseña"};
+		Statement st;
+		ResultSet rs;
+		Connection con = null;
+		try {
+			con = DriverManager.getConnection(
+			"jdbc:h2:mem:test","root","root");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		return inputStreamResource;
+		String query = "SELECT * FROM users";
+		
+		try {
+			st = con.createStatement();
+			rs = st.executeQuery(query);
+			
+			while(rs.next()) {
+				int id = rs.getInt(1);
+				String email = rs.getString(2);
+				String fs = rs.getString(3);
+				String ls = rs.getString(4);
+				String pwd = rs.getString(5);
+				
+				Usuario u = new Usuario(id,email,fs,ls,pwd);
+				usuarios.add(u);
+			}
+		}catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		
+		for(Usuario u : usuarios)
+			System.out.println(u.getId() + " " + u.getEmail() + " " + u.getFirstname() + " " + u.getLastname() + " " + u.getPassword());
+		
+		HSSFWorkbook workbook = new HSSFWorkbook();
+		 HSSFSheet hoja = workbook.createSheet();
+		 CellStyle negrita = workbook.createCellStyle();
+		 negrita.setFillForegroundColor(IndexedColors.BLUE.getIndex());
+		 negrita.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+		 Font fuente = workbook.createFont();
+		 fuente.setBold(true);
+		 fuente.setFontHeightInPoints((short)16);
+		 fuente.setFontName("Calibri");
+		 negrita.setFont(fuente);
+		
+		 
+		
+		 int f = 0;
+		 for (int i = 0; i < usuarios.size()+1; i++) {
+			 HSSFRow fila = hoja.createRow(f++);	
+			 int c = 0;
+			for (int j = 0; j < columnas.length; j++) {
+				HSSFCell celda = fila.createCell(c++);
+				if(f == 1) {
+					celda.setCellStyle(negrita);
+					celda.setCellValue(columnas[j]);
+				}
+				else {
+					if( j == 0)
+						celda.setCellValue(usuarios.get(i-1).getId());
+					else if( j == 1)
+						celda.setCellValue(usuarios.get(i-1).getEmail());
+					else if( j == 2)
+						celda.setCellValue(usuarios.get(i-1).getFirstname());
+					else if( j == 3)
+						celda.setCellValue(usuarios.get(i-1).getLastname());
+					else if( j == 4)
+						celda.setCellValue(usuarios.get(i-1).getPassword());
+				}					
+			}
+
+		 }
+			ByteArrayInputStream inputStreamResource = null;
+			FileOutputStream fos;
+			try {
+				fos = new FileOutputStream("C:\\Users\\fgarcmac\\Desktop\\lista-final-alumnos.xls");
+				//IOUtils.copy(inputStreamResource, new FileOutputStream("C:\\Users\\fgarcmac\\Desktop\\lista-final-alumnos.xls"));
+				 System.out.println("Escribiendo...");
+				 System.out.println("fin");
+				 workbook.write(fos);
+				 workbook.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			return inputStreamResource;
 	}
-	
+
 	@Override
 	public boolean addUsersToDbFromCsvFile(String fileName) {
 		boolean result = true;
 		Resource resource = new ClassPathResource(fileName.concat(Constant.CSV_EXT));
-		
-		//Enunciado: Leer archivo csv para obtener los datos de los Usuarios 
-		//y guardarlos en BBDD
+
+		// Enunciado: Leer archivo csv para obtener los datos de los Usuarios
+		// y guardarlos en BBDD
 		try {
-			//CSVReader reader = new CSVReader(new FileReader(resource.getFile().getPath()));
-			
+			// CSVReader reader = new CSVReader(new
+			// FileReader(resource.getFile().getPath()));
+
 		} catch (Exception e) {
 			result = false;
-		}		
-		
+		}
+
 		return result;
 	}
 
